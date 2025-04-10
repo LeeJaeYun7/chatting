b8006d9ceaaa6aec30f9204a21c2a413c980c073
LeeJongbokZ <s2feeling@naver.com>
2025-04-10 14:39:51 +0900

    Redis Pub/Sub 메시지 발행 실패 시 재시도 로직 추가

--- src/main/java/com/example/chatting/chat/oneonone/message/infrastructure/redis/OneOnOneChatMessageEventPublisherImpl.java
+++ src/main/java/com/example/chatting/chat/oneonone/message/infrastructure/redis/OneOnOneChatMessageEventPublisherImpl.java
@@ -7,6 +7,9 @@ import com.example.chatting.shared.utils.JsonConverter;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RTopic;
 import org.redisson.api.RedissonClient;
+import org.redisson.client.RedisConnectionException;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,11 +18,22 @@ public class OneOnOneChatMessageEventPublisherImpl implements OneOnOneChatMessag
 
     private final JsonConverter jsonConverter;
     private final RedissonClient redissonClient;
-
+    private final RetryTemplate retryTemplate;
+    @Override
     public void publishOneOnOneChatMessageEvent(OneOnOneChatMessageEvent event){
         String message = jsonConverter.convertToJson(event);
         RTopic topic = redissonClient.getTopic(RedisKey.ONEONONE_CHAT_CHANNEL);
-        topic.publish(message);
+
+        retryTemplate.execute(context -> {
+            try {
+                topic.publish(message);  // 메시지 발행
+            } catch (RedisConnectionException e) {
+                // Redis 연결 오류 시 로그를 남기고 재시도
+                // 예외 처리 로직 추가 가능
+                throw e;
+            }
+            return null;
+        });
     }
 }
 
